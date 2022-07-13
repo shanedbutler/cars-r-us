6cd75f7ff2d53404fc73ec0a80fb2c4c746f253c
minidiscs <shanedbutler@gmail.com>
2022-07-13 15:54:10 -0400

    add model module

---
+++ scripts/model.js
@@ -0,0 +1,26 @@
+import { setModel } from "./database.js"
+
+//set state using change listener
+document.addEventListener("change", (changeEvent) => {
+    if (changeEvent.target.id === "model") {
+        setModel(parseInt(changeEvent.target.value))
+    }
+}
+)
+
+export const modelSection = () => {
+    let html = `<section class="choices--model options">`
+
+    html += `
+        <h2>Model</h2>
+        <select id="model">
+            <option value="0">Select a model</option>
+            <option value="1">Car</option>
+            <option value="2">SUV</option>
+            <option value="3">Truck</option>
+        </select>
+    </section>`
+
+    return html
+
+}
